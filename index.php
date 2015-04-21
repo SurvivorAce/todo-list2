9a5852b50875ae2066ef5f2b662362d9b7c600fc
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-04-21 12:18:46 -0700

    Milestone 2!

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Anderson's to Rekt List</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+	
+	</div>
+</body>
+</html>
