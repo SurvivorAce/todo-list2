0ab897879125e09f57a9ff6e9eaf2d8fe2e5acf2
Anderson Nguyen <andersonnguyen126@gmail.com>
2015-04-24 10:10:13 -0700

    Milestone 16!

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<div class="task-list">
 			<ul>
 				<?php require("includes/connect.php");
-				$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+				$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
 				$query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
 				if ($result = $mysqli->query($query)) {
 					$numrows = $result->num_rows;
@@ -18,9 +18,10 @@
 							$task_id = $row['id'];
 							$task_name = $row['task'];
 
-							echo "<li> 
-							<span>'.$task_name'
-							"
+							echo '<li> 
+							<span>' . $task_name . '</span>
+							<img id="' . $task_id . '" class="delete-button" width="10px" src="images/close.svg"/>
+							</li>';
 						}
 					}
 				}
